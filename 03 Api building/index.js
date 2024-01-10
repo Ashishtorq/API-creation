69af4da8c6f92e5dde963a4378484a864325a3f0
Ashistorq <ashishsinghtarwa@gmail.com>
2024-01-11 04:57:56 +0530

    api

--- 03 Api building/index.js
+++ 03 Api building/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 const mongoose = require("mongoose");
+const dataBase = require("./models/crud")
 
 // middleware
 app.use(express.json());
@@ -18,7 +19,28 @@ app.get('/', (req,res)=>{
     res.status(200).send("Server is ok");
 }) 
 
+// get all data
+app.get('/getAll',async(req,res)=>{
+    const data = await dataBase.find();
+    if(!data) return res.send("No record found");
+    else return res.send(data);
+})
+// get one data
+// update
+app.put("/put/:id", async (req,res)=>{
+    let upName = req.body.Name;
+    let upEmail = req.body.Email;
+    let upid = req.params.id;
 
+    const data = await dataBase.findOneAndUpdate(
+        {id:upid},
+        {$set:{Name:upName, Email:upEmail}},
+        {new:true}
+    )
+    res.send(data);
+})
+// create
+// delete
 
 app.listen(PORT, ()=>{
     console.log(`Server is running of PORT ${PORT}`);
